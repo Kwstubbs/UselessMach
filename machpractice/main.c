ab6b2d2f1f8a26130e5f7292177786b9cceca6e7
Kevin Stubbings <kwstubbings@protonmail.com>
2020-01-30 23:58:22 -0800

    Initial Commit

---
+++ machpractice/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  machpractice
+//
+//  Created by Kevin Stubbings on 1/30/20.
+//  Copyright © 2020 Kevin Stubbings. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
